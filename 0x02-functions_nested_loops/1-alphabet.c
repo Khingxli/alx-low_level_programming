3adf6d49765a6fe74085359bd562125de8231418
Kingsley <kingsleythomas885@gmail.com>
2023-06-21 13:58:06 -0700

    rect

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*
+*print_alph: prints the alphabets in lower case
+*Return: Always 0 (success)
+*/
+
+void print_alph(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{_putchar(c);
+
+}
+_putchar('\n');
+}
