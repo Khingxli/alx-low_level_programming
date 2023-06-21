d848393b754adffae8ff0fa5ac4ff679c2256c87
Kingsley <kingsleythomas885@gmail.com>
2023-06-21 13:04:21 -0700

    track

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
-/*
-*
-* function that prints "_putchar"
-*
-*Returns 0
-*/
+/**
+ * main - Entry point
+ * 
+ *function that prints "_putchar"
+ * @_putchar: is the character to print
+ * 
+ * Return: Always 0 (Success)
+ */
 
-int main(void)
+int  main(void)
+
 {
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
