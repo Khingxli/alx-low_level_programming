82eae7dea18c3c7fa69abe73ca8d994165871595
Kingsley <kingsleythomas885@gmail.com>
2023-06-21 14:48:50 -0700

    kk

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints the alphabets 10 times
+*Return: always 0
+*/
+
+int print_alphabet_x10(void)
+{
+char = c = 'a'
+int = i(0)
+
+while (i <= 9)
+{
+for (c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++
+}
+}
