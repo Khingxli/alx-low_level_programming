66a24537436af13074b1a5cc506253e36d092f3e
Kingsley <kingsleythomas885@gmail.com>
2023-06-23 00:21:29 -0700

    code that checks for capitalization of a character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+char _isupper(char c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the repository for More Functions and Nested Loops
