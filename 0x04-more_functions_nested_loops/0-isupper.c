80b276bf04a0b7defcc36e8b3308abd5062072c3
Kingsley <kingsleythomas885@gmail.com>
2023-06-23 00:42:53 -0700

    capitalization

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
- */
-int _isupper(int c)
+* _isupper - checks if parameter is an uppercase character.
+* @c: input character.
+* Return: 1 if is an uppercase character, 0 in other case.
+*/
+char _isupper(char c)
 {
 if (c >= 65 && c <= 90)
 {
