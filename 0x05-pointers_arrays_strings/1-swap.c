37d4dfd9283807a21df6e35dc4ca3f394bcfa6fa
Kingsley <kingsleythomas885@gmail.com>
2023-06-27 17:40:19 -0700

    swaps

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,17 +9,9 @@
 
 void swap_int(int *a, int *b)
 {
-int *a;
-int *b;
-
-*a = 98;
-*b = 42;
-
-printf("*a=%d, *b=%d\n", *a, *b);
-swap_int (&a, &b);
-
-printf("*a=%d, *b=%d\n", *a, *b);
-
+int c = *a;
+*a = *b;
+*b = c;
 return (0);
 
 }
