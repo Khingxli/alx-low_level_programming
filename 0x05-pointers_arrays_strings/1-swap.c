9221c7e8f21ca083592594695acdb57b17e53e0b
Kingsley <kingsleythomas885@gmail.com>
2023-06-27 17:31:40 -0700

    swaps

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*void swap_int - swaps the values of two integers
+*@a: first integer
+*@b: second integer
+*Return: returns 0
+*/
+
+void swap_int(int *a, int *b);
+{
+int *a;
+int *b;
+
+a = 98;
+b = 42;
+
+printf("*a=%d, *b=%d\n", *a, *b);
+swap_int (&a, &b);
+
+printf("*a=%d, *b=%d\n", *a, *b);
+
+return (0);
+
+}
